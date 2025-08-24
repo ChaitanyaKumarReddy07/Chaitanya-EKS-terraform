5983a3a658a65733495bd181836202602dcf9513
Chaitanya Kumar Reddy <157815652+ChaitanyaKumarReddy07@users.noreply.github.com>
2025-08-25 02:27:44 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -6,15 +6,12 @@ terraform {
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
-    bucket         = "dev-chaitanya-tf-bucket"   # use the exact bucket name
-    region         = "ap-southeast-2"              
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "chaitanya-tf-locks"        # exact table name with 's'
+    bucket         = "dev-chaitanya-tf-bucket"    # Ensure this bucket exists in ap-southeast-2
+    region         = "ap-southeast-2"             # Region of the bucket and DynamoDB
+    key            = "eks/terraform.tfstate"      # Path inside the bucket for tfstate
+    dynamodb_table = "chaitanya-tf-locks"         # Ensure the exact table name exists in same region
     encrypt        = true
   }
-}
-
-provider "aws" {
-  region = var.aws-region
 }
